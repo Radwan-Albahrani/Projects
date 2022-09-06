64daa0ead474fbc55a69bb3c47b51921e593fe5d
Radwan-Albahrani <40364775+Radwan-Albahrani@users.noreply.github.com>
2022-09-06 22:48:59 +0300

    Fixed History page manual tampering

--- Web Development/finance/app.py
+++ Web Development/finance/app.py
@@ -4,8 +4,6 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from tempfile import mkdtemp
-import requests
-from sqlalchemy.sql.expression import false
 from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
 from werkzeug.security import check_password_hash, generate_password_hash
 
