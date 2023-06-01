f1e96f4b779aa3eff4e1e827f31f500740d87c38
Radwan-Albahrani <radwan158@gmail.com>
2023-06-01 17:17:13 +0300

    added crash log

--- Python/General Projects/Survey Filler/surveyFiller.py
+++ Python/General Projects/Survey Filler/surveyFiller.py
@@ -6,6 +6,8 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.service import Service as EdgeService
 from time import sleep
 from pwinput import pwinput
+import os
+import logging
 
 
 
@@ -24,105 +26,122 @@ password = pwinput("Enter your Password: ")
 browser.find_element(By.ID, 'userNameInput').send_keys(username)
 browser.find_element(By.ID, 'passwordInput').send_keys(password)
 browser.find_element(By.ID, 'submitButton').click()
-
-try:
-    browser.find_element(
-        By.XPATH, "/html/body/form/div[5]/header/div/div[3]/ul/li[1]")
-    print("Successfully logged in")
-except:
-    print("Login Failed")
-    exit(1)
-while True:
-    speedup = input("Would you like to speed up the process? (y/n): ")
-    score = 0
-    if speedup == 'y':
-        while True:
-            score = input(
-                "How would you like to rate All courses and Instructors: ")
-            if int(score) < 1 or int(score) > 5:
-                print("Please enter a valid score")
-                continue
-            else:
-                break
-        break
-    elif speedup == 'n':
+def programRoutine():
+    try:
+        browser.find_element(
+            By.XPATH, "/html/body/form/div[5]/header/div/div[3]/ul/li[1]")
+        print("Successfully logged in")
+    except:
+        print("Login Failed")
+        exit(1)
+    while True:
+        speedup = input("Would you like to speed up the process? (y/n): ")
         score = 0
-        break
-    else:
-        print("Please enter a valid input")
-while True:
-    try:
-        browser.find_element(
-            By.ID, f'cphMainContent_gvCesSurveysToFill_lbnCourseName_0').click()
-        sleep(0.1)
-        title = browser.find_element(
-            By.ID, 'cphMainContent_lblCourseTitle').text
-        if speedup == 'n':
-            print(f"The course title is: {title}")
+        if speedup == 'y':
             while True:
-                score = input("How would you like to rate this course: ")
-                if score < 1 or score > 5:
+                score = input(
+                    "How would you like to rate All courses and Instructors: ")
+                if int(score) < 1 or int(score) > 5:
                     print("Please enter a valid score")
                     continue
                 else:
                     break
-        allInputs = browser.find_elements(By.TAG_NAME, 'input')
-        for i in allInputs:
-            try:
-                if i.get_attribute('type') == 'text':
-                    i.send_keys(score)
-            except Exception as e:
-                continue
-        browser.find_element(By.ID, 'a_next').click()
-        sleep(1)
-        allInputs = browser.find_elements(By.TAG_NAME, 'input')
-        for i in allInputs:
-            try:
-                if i.get_attribute('type') == 'text':
-                    i.send_keys(score)
-            except Exception as e:
-                continue
-        browser.find_element(By.ID, 'cphMainContent_lbnSubmitCES').click()
-        sleep(0.1)
-    except Exception as e:
-        print("Done With Courses!")
-        break
+            break
+        elif speedup == 'n':
+            score = 0
+            break
+        else:
+            print("Please enter a valid input")
+    while True:
+        try:
+            browser.find_element(
+                By.ID, f'cphMainContent_gvCesSurveysToFill_lbnCourseName_0').click()
+            sleep(0.1)
+            title = browser.find_element(
+                By.ID, 'cphMainContent_lblCourseTitle').text
+            if speedup == 'n':
+                print(f"The course title is: {title}")
+                while True:
+                    score = input("How would you like to rate this course: ")
+                    if score < 1 or score > 5:
+                        print("Please enter a valid score")
+                        continue
+                    else:
+                        break
+            allInputs = browser.find_elements(By.TAG_NAME, 'input')
+            for i in allInputs:
+                try:
+                    if i.get_attribute('type') == 'text':
+                        i.send_keys(score)
+                except Exception as e:
+                    continue
+            browser.find_element(By.ID, 'a_next').click()
+            sleep(1)
+            allInputs = browser.find_elements(By.TAG_NAME, 'input')
+            for i in allInputs:
+                try:
+                    if i.get_attribute('type') == 'text':
+                        i.send_keys(score)
+                except Exception as e:
+                    continue
+            browser.find_element(By.ID, 'cphMainContent_lbnSubmitCES').click()
+            sleep(0.1)
+        except Exception as e:
+            print("Done With Courses!")
+            break
 
-while True:
-    try:
-        browser.find_element(
-            By.ID, f'cphMainContent_gvSSLSSurveysToFill_lbnTeacher_0').click()
-        sleep(1)
-        title = browser.find_element(By.ID, 'cphMainContent_lblFaculty').text
-        if speedup == "n":
-            print(f"The Instructor is: {title}")
-            score = input("How would you like to rate this Instructor: ")
-            while True:
-                score = input("How would you like to rate this course: ")
-                if score < 1 or score > 5:
-                    print("Please enter a valid score")
+    while True:
+        try:
+            browser.find_element(
+                By.ID, f'cphMainContent_gvSSLSSurveysToFill_lbnTeacher_0').click()
+            sleep(1)
+            title = browser.find_element(By.ID, 'cphMainContent_lblFaculty').text
+            if speedup == "n":
+                print(f"The Instructor is: {title}")
+                score = input("How would you like to rate this Instructor: ")
+                while True:
+                    score = input("How would you like to rate this course: ")
+                    if score < 1 or score > 5:
+                        print("Please enter a valid score")
+                        continue
+                    else:
+                        break
+            allInputs = browser.find_elements(By.TAG_NAME, 'input')
+            for i in allInputs:
+                try:
+                    if i.get_attribute('type') == 'text':
+                        i.send_keys(score)
+                except Exception as e:
                     continue
-                else:
-                    break
-        allInputs = browser.find_elements(By.TAG_NAME, 'input')
-        for i in allInputs:
-            try:
-                if i.get_attribute('type') == 'text':
-                    i.send_keys(score)
-            except Exception as e:
-                continue
-        browser.find_element(By.ID, 'a_next').click()
-        sleep(1)
-        allInputs = browser.find_elements(By.TAG_NAME, 'input')
-        for i in allInputs:
-            try:
-                if i.get_attribute('type') == 'text':
-                    i.send_keys(score)
-            except Exception as e:
-                continue
-        browser.find_element(By.ID, 'cphMainContent_lbnSubmitSSLS').click()
-        sleep(1)
+            browser.find_element(By.ID, 'a_next').click()
+            sleep(1)
+            allInputs = browser.find_elements(By.TAG_NAME, 'input')
+            for i in allInputs:
+                try:
+                    if i.get_attribute('type') == 'text':
+                        i.send_keys(score)
+                except Exception as e:
+                    continue
+            browser.find_element(By.ID, 'cphMainContent_lbnSubmitSSLS').click()
+            sleep(1)
+        except Exception as e:
+            print("Done With Instructors!")
+            sleep(2)
+            browser.close()
+            break
+
+if __name__ == "__main__":
+    try:
+        programRoutine()
     except Exception as e:
-        print("Done With Instructors!")
-        sleep(2)
-        break
+        try:
+            sleep(0.1)
+            os.mkdir("CrashLog")
+        except FileExistsError:
+            pass
+    logging.disable(logging.NOTSET)
+    logging.basicConfig(filename=r"CrashLog/ErrorLog.log", level=logging.DEBUG,
+                        format='%(asctime)s %(levelname)s %(name)s %(message)s', force=True)
+    logging.exception(e)
+    print("Error has been logged in a file called \"CrashLogs\". Please Send it to creator to fix this crash!")
+    sleep(3)
